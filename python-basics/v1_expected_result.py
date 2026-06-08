92b817143d7d1d9c92652630375f96cf3ea0becc
shivamnamdev <shivamnamdev.edu@gmail.com>
2026-06-08 18:17:25 +0530

    Update expected solution actually

---
+++ python-basics/v1_expected_result.py
@@ -0,0 +1,112 @@
+
+# Hey everyone! 👋
+
+# Your Python Basics Final Assignment is now live. This is a comprehensive assignment designed to test everything you've learned so far.
+
+# 📌 What is this assignment?
+# This assignment contains 5 advanced-level questions that combine multiple Python concepts into real-world mini-projects. It's not just about writing code — it's about thinking logically and applying concepts together.
+
+# -------------------------
+
+# Expected Output:
+# Topper: Jaskeerat (Avg: 87.0)
+# Lowest: Amit (Avg: 59.0)
+# Students with 3+ subjects above 80: ["Smeeta", "Jaskeerat"]
+# Sorted by average: ['Jaskeerat', 'Smeeta', 'Rahul', 'Niraj', 'Amit']
+# Improvement Report
+# Rahul -> Yes
+# Smeeta -> No
+# Amit -> No
+# Jaskeerat -> Yes
+# Niraj -> Yes
+
+# -------------------------
+
+# Expected Output:
+# First 4: Pyth 
+# Has uppercase: Yes
+# Has digit: No
+
+# Last 4: 025!
+# Has uppercase: No
+# Has digit: Yes
+
+# Character Counts
+# Uppercase: 2
+# Lowercase: 9
+# Digits: 5
+# Special: 2
+
+# Repeated patterns found: oo
+# find(): 10
+# index(): 10
+# count(): 2
+# replace(): Pyth0n@Sch**l2025!
+
+# Reversed Password:
+# !5202loohcS@n0htyP
+# Palindrome substrings: ['202']
+
+# Masked Password:
+# Py**************5!
+
+# Rating: Medium
+
+# -------------------------
+
+# Expected Output:
+# IT Average Salary = 37500.0
+# HR Average Salary = 40000.0
+# Finance Average Salary = 60000.0
+
+# Highest Paying Department: Finance
+# Average Salary: 60000.0
+
+# Employee 103 transferred to Finance
+# Employee 105 removed
+# Employee 101 promoted
+# Attendance popped from Smeeta: 21
+
+# FINAL PAYSLIPS
+
+# Employee ID: 101
+# {'name': 'Rahul', 'gross': 55000, 'deductions': 1000, 'net': 54000, 'band': 'Senior'}
+# Employee ID: 102
+# {'name': 'Priya', 'gross': 55000, 'deductions': 0, 'net': 55000, 'band': 'Senior'}
+# Employee ID: 103
+# {'name': 'Amit', 'gross': 30000, 'deductions': 2000, 'net': 28000, 'band': 'Mid'}
+# Employee ID: 104
+# {'name': 'Smeeta', 'gross': 60000, 'deductions': 500, 'net': 59500, 'band': 'Senior'}
+
+
+# -------------------------
+
+
+# -------------------------
+
+# Expected Output
+# Dr. Sharma - Cardiology
+# Dr. Verma - Neurology
+# Patient: Rahul - Heart Block
+# Patient: Priya - Migraine
+# Rahul's total bill: 23000
+# Dr. Sharma's age: 45
+# Cardiology
+# Dr. Sharma is on duty.
+# Patient Rahul checked in.
+# Hospital system running...
+
+# Doctor Report
+# {'name': 'Dr. Sharma', 'specialization': 'Cardiology', 'patients_count': 1, 'available_days': {'Thursday', 'Monday'}}
+
+# Discount
+# 2300.0
+
+# Cabin Details
+# Dr. Sharma - Floor: 2, Room: 5
+
+# Discharge Patient
+# Priya discharged successfully
+
+# Updated Patient List
+# Patient: Rahul - Heart Block
